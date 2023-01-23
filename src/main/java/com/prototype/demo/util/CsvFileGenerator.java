4aad330f20125b81979499d1916ca6fe57206f6a
pj <119717661@umail.ucc.ie>
2023-01-23 17:53:45 +0000

    Fixed Employee Writer

--- src/main/java/com/prototype/demo/util/CsvFileGenerator.java
+++ src/main/java/com/prototype/demo/util/CsvFileGenerator.java
@@ -12,16 +12,21 @@ import com.prototype.demo.model.Employee;
 
 @Component
 public class CsvFileGenerator {
-public void writeStudentsToCsv(List<Employee> employees, Writer writer) {
+
+    //Employee Writer
+public void writeEmployeesToCsv(List<Employee> employees, Writer writer) {
 try {
 
-CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT);
-for (Employee employee : employees) {
-printer.printRecord(employee.geteID(), employee.getEfName(), employee.getElName(),
-employee.geteType());
+try (CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT)) {
+    for (Employee employee : employees) {
+    printer.printRecord(employee.geteID(), employee.getEfName(), employee.getElName(),
+    employee.geteType());
+    }
 }
 } catch (IOException e) {
 e.printStackTrace();
 }
 }
+
+
 }
