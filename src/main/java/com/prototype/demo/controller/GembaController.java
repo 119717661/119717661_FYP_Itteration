53a35e19001355b6c0487afde379bb6f117a0ab1
pj <119717661@umail.ucc.ie>
2023-02-21 19:44:44 +0000

    finallyfinWORKINGBIACZ

--- src/main/java/com/prototype/demo/controller/GembaController.java
+++ src/main/java/com/prototype/demo/controller/GembaController.java
@@ -1,12 +1,10 @@
 package com.prototype.demo.controller;
 
-import javax.servlet.ServletRequest;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,61 +45,44 @@ GembaListRepo gembaListRepository;
         return "gemba";
     }
 
-    @RequestMapping("/viewGemba")
+
+    @GetMapping("/viewGemba")
     public String viewGemba(Model model) {
-        model.addAttribute("gemba", new Gemba());
+        model.addAttribute("gembaLists", gembaListRepository.findAll());
+        model.addAttribute("gemba", gembaRepository.findAll());
         return "viewGemba";
     }
-
-//Take a look at this PostMapping method below, it saves the weekNumber but it does not save the information from the table form 
-//Provide fix for this method knowing it needs take information from gemba.html form :
-/*<form th:action="@{/saveGembaData}" th:method="post">
-  <input type="text" class="form-control" id="weekNumber" name="weekNumber" required>
-  <table id="taskTable" class="table table-striped" style="display: none">
-    <tr>
-      <th>Task ID</th>
-      <th>Task Name</th>
-      <th>Process Name</th>
-      <th>Time of Task</th>
-      <th>Date</th>
-    </tr>
-    <tr th:each="task : ${tasks}">
-      <td th:text="${task.taskid}"></td>
-      <td th:text="${task.taskname}"></td>
-      <td th:text="${task.process.procName}" class="process-name"></td>
-      <td class="time-of-task" id="time-of-task-{{task.taskid}}"></td>
-      <td class="todayday" th:text="${date}"></td>
-    </tr>
     
-  </table>
-  <div class="form-group">
-      <button type="submit" id="submitformbutton" class="btn btn-primary">Save Times of Task for This Process</button>
-  </div>
-  </form>
- */
-    @PostMapping("/saveGembaData")
-    public String saveSchedule(@RequestParam("weekNumber") String weekNumber,
-     ServletRequest request) {
-        
-    GembaList gembaList = new GembaList();
-    gembaList.setListNumber(weekNumber);
-    gembaListRepository.save(gembaList);
-   
-       return "redirect:/viewGemba";
-    }
 
-    public void tabledataSave(@RequestParam("taskID") String taskID,
-            @RequestParam("employee") String employee,
-            @RequestParam("processName") String processName,
-            @RequestParam("timeOfTask") String timeOfTask) {
-   
-        Gemba gemba = new Gemba();
-        gemba = new Gemba();
-        gemba.setTaskID(taskID);
-        gemba.setTaskname(employee);
-        gemba.setProcessName(processName);
-        gemba.setTimeOfTask(timeOfTask);
-        gembaRepository.save(gemba);
-    }
+
+
+  @PostMapping("/saveGembaData")
+  public String saveSchedule(@RequestParam("weekNumber") String weekNumber, @RequestParam("taskid") List<String> taskIds,
+      @RequestParam("taskname") List<String> taskNames, @RequestParam("processName") List<String> processNames, 
+      @RequestParam("timeOfTask") List<String> timesOfTask, @RequestParam("date") List<String> dates) {
+      
+      // Create a new GembaList and save it
+      GembaList gembaList = new GembaList();
+      gembaList.setListNumber(weekNumber);
+      gembaListRepository.save(gembaList);
+    
+      // Create a Gemba record for each task and save it
+      for (int i = 0; i < taskIds.size(); i++) {
+          Gemba gemba = new Gemba();
+          gemba.setTaskID(taskIds.get(i));
+          gemba.setTaskname(taskNames.get(i));
+          gemba.setProcessName(processNames.get(i));
+          gemba.setTimeOfTask(timesOfTask.get(i));
+          gemba.setDate(dates.get(i));
+          gemba.setGembaList(gembaList); // set the GembaList for each Gemba record
+          gembaRepository.save(gemba);
+      }
+      
+      return "redirect:/viewGemba";
+  }
+  
+
+
+
     
 }
