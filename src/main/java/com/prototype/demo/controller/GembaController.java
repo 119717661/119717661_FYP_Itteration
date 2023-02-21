b7506e0eb0849ade2860059d89a433d6a27cfbfb
119717661 <119717661@umail.ucc.ie>
2023-02-21 14:42:15 +0000

    newtest

--- src/main/java/com/prototype/demo/controller/GembaController.java
+++ src/main/java/com/prototype/demo/controller/GembaController.java
@@ -1,18 +1,20 @@
 package com.prototype.demo.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.prototype.demo.dao.GembaListRepo;
 import com.prototype.demo.dao.GembaRepo;
 import com.prototype.demo.model.Gemba;
-import com.prototype.demo.model.Task;
+import com.prototype.demo.model.GembaList;
 import com.prototype.demo.service.GembaService;
 import com.prototype.demo.service.ProcessService;
 import com.prototype.demo.service.TaskService;
@@ -28,7 +30,8 @@ public class GembaController {
     GembaService gembaService;
     @Autowired
     GembaRepo gembaRepository;
-
+@Autowired
+GembaListRepo gembaListRepository;
     
 
     @RequestMapping("/gemba")
@@ -44,30 +47,61 @@ public class GembaController {
         return "gemba";
     }
 
-    
-    @PostMapping("/saveGemba")
-public ModelAndView saveGemba(Gemba gemba, HttpServletRequest request) {
-Integer maxGembaId = gembaRepository.findMaxGembaID();
-if (maxGembaId == null) {
-maxGembaId = 0;
-}
-for (Task task : taskService.getAllTasks()) {
-Gemba newGemba = new Gemba();
-newGemba.setGembaID(maxGembaId + 1);
-newGemba.setTaskID(task.getTaskid());
-newGemba.setTaskName(task.getTaskname());
-newGemba.setProcessName(task.getProcess().getProcName());
-newGemba.setTimeOfTask(request.getParameter("time-of-task-" + task.getTaskid()));
-gembaService.saveGemba(newGemba);
-maxGembaId++;
-}
-return new ModelAndView("redirect:/viewgemba");
-}
-    
-    @GetMapping("/viewgemba")
+    @RequestMapping("/viewGemba")
     public String viewGemba(Model model) {
-        model.addAttribute("gembas", gembaService.getAllGembas());
-        return "viewgemba";
+        model.addAttribute("gemba", new Gemba());
+        return "viewGemba";
+    }
+
+//Take a look at this PostMapping method below, it saves the weekNumber but it does not save the information from the table form 
+//Provide fix for this method knowing it needs take information from gemba.html form :
+/*<form th:action="@{/saveGembaData}" th:method="post">
+  <input type="text" class="form-control" id="weekNumber" name="weekNumber" required>
+  <table id="taskTable" class="table table-striped" style="display: none">
+    <tr>
+      <th>Task ID</th>
+      <th>Task Name</th>
+      <th>Process Name</th>
+      <th>Time of Task</th>
+      <th>Date</th>
+    </tr>
+    <tr th:each="task : ${tasks}">
+      <td th:text="${task.taskid}"></td>
+      <td th:text="${task.taskname}"></td>
+      <td th:text="${task.process.procName}" class="process-name"></td>
+      <td class="time-of-task" id="time-of-task-{{task.taskid}}"></td>
+      <td class="todayday" th:text="${date}"></td>
+    </tr>
+    
+  </table>
+  <div class="form-group">
+      <button type="submit" id="submitformbutton" class="btn btn-primary">Save Times of Task for This Process</button>
+  </div>
+  </form>
+ */
+    @PostMapping("/saveGembaData")
+    public String saveSchedule(@RequestParam("weekNumber") String weekNumber,
+     ServletRequest request) {
+        
+    GembaList gembaList = new GembaList();
+    gembaList.setListNumber(weekNumber);
+    gembaListRepository.save(gembaList);
+   
+       return "redirect:/viewGemba";
+    }
+
+    public void tabledataSave(@RequestParam("taskID") String taskID,
+            @RequestParam("employee") String employee,
+            @RequestParam("processName") String processName,
+            @RequestParam("timeOfTask") String timeOfTask) {
+   
+        Gemba gemba = new Gemba();
+        gemba = new Gemba();
+        gemba.setTaskID(taskID);
+        gemba.setTaskname(employee);
+        gemba.setProcessName(processName);
+        gemba.setTimeOfTask(timeOfTask);
+        gembaRepository.save(gemba);
     }
     
-    }
+}
